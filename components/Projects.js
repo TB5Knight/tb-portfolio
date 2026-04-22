a084d3937c019745f25c26bd44517b68d0f008f8
TB5Knight <ttburdgess@gmail.com>
2026-04-22 15:10:47 -0400

    commit

--- components/Projects.js
+++ components/Projects.js
@@ -1,93 +1,128 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView,Image,Linking} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, ScrollView, Image, Linking } from 'react-native';
 import { Button } from '@rneui/themed';
+
+import Navbar from './Navbar';
+import Footer from './Footer';
+
 import project1 from './img/excercise-app.png';
 import project2 from './img/reminderpilot.png';
 import project3 from './img/stayfitproject.png';
 
-const Projects = ({ innerRef }) => {
-  const navigation = useNavigation();
+const Projects = () => {
   return (
-    <ScrollView ref={innerRef} style={styles.container}>
-      <Text style={styles.title}>Projects</Text>
+    <View style={styles.container}>
 
-      {/* Exercise App */}
+      {/* NAVBAR */}
+      <Navbar />
+
+      {/* CONTENT */}
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+
+        <Text style={styles.title}>Projects</Text>
+
+        {/* Exercise App */}
         <View style={styles.project}>
-            <Image source={project1} style={styles.image} />
-            <Text style={styles.projectTitle}>Exercise App</Text>
-            <Text style={styles.projectDesc}>A exercise app designed to help users track their workouts and stay motivated. This was designed with React.</Text>
-           <View style={styles.buttonContainer}>
-           <Button
-            title="View the Link"onPress={() => Linking.openURL('https://tb5knight.github.io/fit-app/')  }
-           buttonStyle={styles.button} />
-           </View>
+          <Image source={project1} style={styles.image} />
+          <Text style={styles.projectTitle}>Exercise App</Text>
+          <Text style={styles.projectDesc}>
+            A exercise app designed to help users track their workouts and stay motivated.
+          </Text>
+
+          <View style={styles.buttonContainer}>
+            <Button
+              title="View the Link"
+              onPress={() => Linking.openURL('https://tb5knight.github.io/fit-app/')}
+              buttonStyle={styles.button}
+            />
+          </View>
         </View>
 
-      {/* Reminder Pilot */}
+        {/* Reminder Pilot */}
         <View style={styles.project}>
-            <Image source={project2} style={styles.image} />
-            <Text style={styles.projectTitle}>Reminder Pilot</Text>
-            <Text style={styles.projectDesc}>A reminder app designed to help users stay organized and on top of their tasks. This was designed with React native.</Text>
-           <View style={styles.buttonContainer}>
-           <Button
-            title="View the Link"onPress={() => Linking.openURL('https://tb5knight.github.io/reminder-pilot/')  }
-           buttonStyle={styles.button} />
-           </View>
+          <Image source={project2} style={styles.image} />
+          <Text style={styles.projectTitle}>Reminder Pilot</Text>
+          <Text style={styles.projectDesc}>
+            A reminder app designed to help users stay organized and on top of their tasks.
+          </Text>
+
+          <View style={styles.buttonContainer}>
+            <Button
+              title="View the Link"
+              onPress={() => Linking.openURL('https://tb5knight.github.io/reminder-pilot/')}
+              buttonStyle={styles.button}
+            />
+          </View>
         </View>
 
         {/* Stay Fit Project */}
         <View style={styles.project}>
-            <Image source={project3} style={styles.image} />
-            <Text style={styles.projectTitle}>Stay Fit Project</Text>
-            <Text style={styles.projectDesc}>A fitness app designed to help users achieve their health and wellness goals. This was designed with React native</Text>
-           <View style={styles.buttonContainer}>
-           <Button
-            title="View the Link"onPress={() => Linking.openURL('https://tb5knight.github.io/stay-fit/')  }
-           buttonStyle={styles.button} />
-           </View>
+          <Image source={project3} style={styles.image} />
+          <Text style={styles.projectTitle}>Stay Fit Project</Text>
+          <Text style={styles.projectDesc}>
+            A fitness app designed to help users achieve their health and wellness goals.
+          </Text>
+
+          <View style={styles.buttonContainer}>
+            <Button
+              title="View the Link"
+              onPress={() => Linking.openURL('https://tb5knight.github.io/stay-fit/')}
+              buttonStyle={styles.button}
+            />
+          </View>
         </View>
-    </ScrollView>
-    );
-    };
+
+      </ScrollView>
+
+      {/* FOOTER */}
+      <Footer />
+
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: { 
-    padding: 30,
+  container: {
+    flex: 1,
     backgroundColor: '#15616d'
   },
-  title: { 
-    color: '#ffff',
-    fontSize: 22, 
-    fontWeight: 'bold', 
-    marginBottom: 20 
+  scrollContent: {
+    padding: 30,
+    alignItems: 'center'
   },
-  project: { 
-    marginBottom: 30, 
-    alignItems: 'center' 
+  title: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 20
   },
-  image: { 
-    width: 300, 
-    height: 200, 
-    marginBottom: 10 
+  project: {
+    marginBottom: 30,
+    alignItems: 'center'
   },
-button: {
-  backgroundColor: '#14213d',
-},
-buttonContainer: {
-  borderRadius: 8,
-  paddingVertical: 10,
-  paddingHorizontal: 15
-},
-
- projectTitle: { 
-    color: '#ffff',
-    fontSize: 18, 
-    fontWeight: 'bold' 
+  image: {
+    width: 300,
+    height: 200,
+    marginBottom: 10,
+    borderRadius: 10
   },
-  projectDesc: { 
-    color: '#ffff',
-    textAlign: 'center', 
-    marginBottom: 10 
+  projectTitle: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  projectDesc: {
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: '#14213d',
+    borderRadius: 8
+  },
+  buttonContainer: {
+    marginTop: 10,
+    width: '80%'
   }
 });
 export default Projects;
