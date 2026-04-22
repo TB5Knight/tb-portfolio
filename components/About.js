fbe83babed72ae7017200170470c71b4b9251908
TB5Knight <ttburdgess@gmail.com>
2026-04-22 14:02:39 -0400

    commit

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
-import profile from './img/AI-image.JPG';
+import profile from './img/profile.jpg';
 
 const About = () => {
   return (
