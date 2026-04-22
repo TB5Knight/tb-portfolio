bb72f2cb7fa8d8ceef5c79d7da43d229979b8382
TB5Knight <ttburdgess@gmail.com>
2026-04-22 13:58:26 -0400

    commit

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import { Button } from '@rneui/themed';
-import background from './components/img/background.png';
+import background from './img/background.png';
 
 const Home = ({ navigation }) => {
   return (
